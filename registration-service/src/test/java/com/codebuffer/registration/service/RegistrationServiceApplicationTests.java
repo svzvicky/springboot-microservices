4c9ebe9060d57808c262633023c0ac31de4aea68
Vignesh <svzvicky@gmail.com>
2021-05-02 08:54:24 +0530

    Folder Structure Commit

---
+++ registration-service/src/test/java/com/codebuffer/registration/service/RegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebuffer.registration.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
