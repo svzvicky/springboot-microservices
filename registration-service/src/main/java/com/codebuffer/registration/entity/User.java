1e3eb1861c9e5b8f96a961b50e9b1913f63e98e7
Vignesh <svzvicky@gmail.com>
2021-05-06 11:31:52 +0530

    Removed unused imports

--- registration-service/src/main/java/com/codebuffer/registration/entity/User.java
+++ registration-service/src/main/java/com/codebuffer/registration/entity/User.java
@@ -1,13 +1,9 @@
 package com.codebuffer.registration.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 
 @Entity
 public class User {
